ab0b3cd28d557bb9a2d2ee09827ed2d1ffbb4101
Michael Tang <mtang2021@gmail.com>
2024-09-30 18:27:41 -0400

    Fixed audio settings, hooked level buttons to char. select
    Bug fixes

--- audio/audio-settings.js
+++ audio/audio-settings.js
@@ -4,17 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     var volumeValue = document.getElementById("gameVolume");
     volumeValue.innerHTML = volumeSlider.value;
 
+    const sfxSlider = document.getElementById('sfxVolumeSlider');
+    var sfxValue = document.getElementById("sfxVolume");
+    sfxValue.innerHTML = sfxSlider.value;
+
      // necessary as original mp3 is loud
     const factor = 0.005;
     
     // check for slider changes
-     volumeSlider.addEventListener('input', (event) => {
+    volumeSlider.addEventListener('input', (event) => {
         const volume = parseFloat(event.target.value);
 
         if (window.globalAudio) {
             // console.log("setting volume value", volume);
             window.globalAudio.volume = volume * factor;
-            volumeValue.innerHTML = Math.round(volume * 100);
+            volumeValue.innerHTML = Math.round(volume);
         }
         // user interaction required for audio play
         if (window.globalAudio.muted == true) {
@@ -22,6 +26,16 @@ document.addEventListener('DOMContentLoaded', () => {
             window.globalAudio.play().catch(error => {
                 console.error("Failed to play audio:", error);
             });
+        }
+    });
+
+    sfxSlider.addEventListener('input', (event) => {
+        const sfxVolume = parseFloat(event.target.value);
+
+        if (window.globalAudio) {
+            // console.log("setting volume value", volume);
+            window.globalAudio.volume = sfxVolume * factor;
+            sfxValue.innerHTML = Math.round(sfxVolume);
         }
     });
 });
