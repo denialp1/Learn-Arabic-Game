353018b04a4e3a567a4058164be51d17e51be6d5
Michael Tang <mtang2021@gmail.com>
2024-11-10 23:25:10 -0500

    Arabic Book Pages

--- src/book/ArabicBook.js
+++ src/book/ArabicBook.js
@@ -8,8 +8,20 @@ let currentFrame = 0;
 const spriteDiv = document.getElementById("spriteDiv");
 const leftArr = document.getElementById("leftArr");
 const rightArr = document.getElementById("rightArr");
-const bookText = document.getElementById("bookText");
+const bookTextL = document.getElementById("bookTextL");
+const bookTextR = document.getElementById("bookTextR");
 const overlay = document.getElementById('overlay');
+
+var page = 0;
+const pageTexts = [
+  "The Levant refers to the region of the Eastern Mediterranean littoral, and includes modern day Palestine, Syria, Jordan, Lebanon and parts of Turkey and Egypt (the Sinai).",
+  "In Arabic, this region is referred to as الشام (Aş-Sām), which is where the demonym شاميّ (Şāmī) comes from, referring to the people and the dialects that come from this region.",
+  "This region is home to the historical Biblical lands and contains many holy sites for the three biggest Abrahamic religions – Judaism, Christianity and Islam.",
+  "These include Jerusalem (القدس), Bethlehem (بيت لحم), the Jordan River (النھر الأردن), The Temple Mount (where the al-Aqsa Mosque (مسجد الأقصی)) is located.",
+  "This region also contains many other important historical cities and sites like Palmyra (تدمر), Damascus (دمشق), Deir ez-Zor (دير الزور), Tyre (صور) and Mount Lebanon (جبل لبنان). "
+];
+const pages = 5;
+var index = Math.abs(page % pages);
 
 // Set initial styles for the sprite animation div
 spriteDiv.style.backgroundImage = 'url("../assets/book/book.png")';
@@ -51,50 +63,81 @@ function playAnimation(animation, callback) {
 }
 
 function flipLeft() {
-  bookText.style.visibility = "hidden";
+  if (page - 2 < 0) {
+    return;
+  }
+  page -= 2;
+  bookTextL.style.visibility = "hidden";
+  bookTextR.style.visibility = "hidden";
   playAnimation(animations.flipLeft, function () {
-    bookText.style.visibility = "visible";
+    setBookText(bookTextL, pageTexts[page % pages]);
+    console.log(page % pages)
+    setBookText(bookTextR, pageTexts[(page + 1) % pages]);
+    console.log((page + 1) % pages)
+    bookTextL.style.visibility = "visible";
+    bookTextR.style.visibility = "visible";
   });
 }
 
 function flipRight() {
-  bookText.style.visibility = "hidden";
+  if (page + 2 > pages) {
+    return;
+  }
+  page += 2;
+  bookTextL.style.visibility = "hidden";
+  bookTextR.style.visibility = "hidden";
   playAnimation(animations.flipRight, function () {
-    bookText.style.visibility = "visible";
+    setBookText(bookTextL, pageTexts[page % pages]);
+    if (page + 2 > pages) {
+      setBookText(bookTextR, "");
+    } else {
+      setBookText(bookTextR, pageTexts[page + 1 % pages]);
+    }
+    bookTextL.style.visibility = "visible";
+    bookTextR.style.visibility = "visible";
+    console.log(page % pages);
   });
 
 }
 
 function toggleVisibility() {
-  if (bookText.style.visibility === "visible") {
-      bookText.style.visibility = "hidden";
+  if (bookTextL.style.visibility === "visible") {
+      bookTextL.style.visibility = "hidden";
+      bookTextR.style.visibility = "hidden";
       leftArr.style.visibility = "hidden";
       rightArr.style.visibility = "hidden";
       overlay.style.display = 'none';
   } else {
-      bookText.style.visibility = "visible";
+      bookTextL.style.visibility = "visible";
+      bookTextR.style.visibility = "visible";
       leftArr.style.visibility = "visible";
       rightArr.style.visibility = "visible";
       overlay.style.display = 'block';
   }
 }
 
-function setBookText(input) {
-  bookText.textContent = input;
+function setBookText(page, input) {
+  page.textContent = input;
 }
 
 spriteDiv.addEventListener("click", function() { 
     if (isOpen) {
-      toggleVisibility();
       playAnimation(animations.close, function () {
         spriteDiv.className = 'book'; // change to small mode after close
         isOpen = false; // set state close
       });
+      toggleVisibility();
     } else {
       spriteDiv.className = 'book-enlarge'; // change to large mode
       playAnimation(animations.open, function () {
+        setBookText(bookTextL, pageTexts[page % pages]);
+        if ((page + 1) % pages === 0) {
+          setBookText(bookTextR, "")
+        } else {
+          setBookText(bookTextR, pageTexts[(page + 1) % pages]);
+        }
         toggleVisibility();
-        setBookText("Placeholder!");
+        console.log(page % pages);
       });
       isOpen = true; // set state open
     }
