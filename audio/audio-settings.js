c8c01f9de4dd810af76ce4f6378451b8b09991b0
denialp1 <greenleopard2000@gmail.com>
2024-12-09 18:00:51 -0500

    comment out sfx slider

--- audio/audio-settings.js
+++ audio/audio-settings.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const volumeSlider = document.getElementById('gameVolumeSlider');
     const volumeValue = document.getElementById("gameVolume");
-    const sfxSlider = document.getElementById('sfxVolumeSlider');
-    const sfxValue = document.getElementById("sfxVolume");
+    // const sfxSlider = document.getElementById('sfxVolumeSlider');
+    // const sfxValue = document.getElementById("sfxVolume");
 
-    const MAX_VOLUME = 0.5;
+    const MAX_VOLUME = 0.06;
 
     const scaleToAudioVolume = (sliderValue) => Math.pow(sliderValue / 100, 2) * MAX_VOLUME;
     const scaleToSliderValue = (audioVolume) => Math.sqrt(audioVolume / MAX_VOLUME) * 100;
 
     // stored scaled values or use defaults
     const storedVolume = parseFloat(localStorage.getItem('gameVolumeScaled')) || 0.25; // 25% volume
-    const storedSfxVolume = parseFloat(localStorage.getItem('sfxVolumeScaled')) || 0.25; // 25% SFX volume
+    // const storedSfxVolume = parseFloat(localStorage.getItem('sfxVolumeScaled')) || 0.25; // 25% SFX volume
     const initialSliderVolume = scaleToSliderValue(storedVolume);
-    const initialSliderSfxVolume = scaleToSliderValue(storedSfxVolume);
+    // const initialSliderSfxVolume = scaleToSliderValue(storedSfxVolume);
 
     // set initial slider positions, display values
     volumeSlider.value = initialSliderVolume;
     volumeValue.innerHTML = Math.round(initialSliderVolume);
-    sfxSlider.value = initialSliderSfxVolume;
-    sfxValue.innerHTML = Math.round(initialSliderSfxVolume);
+    // sfxSlider.value = initialSliderSfxVolume;
+    // sfxValue.innerHTML = Math.round(initialSliderSfxVolume);
 
     // set initial audio volume
     if (window.globalAudio) {
@@ -46,16 +46,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    sfxSlider.addEventListener('input', (event) => {
-        const sliderValue = parseFloat(event.target.value);
-        const scaledSfxVolume = scaleToAudioVolume(sliderValue);
+    // sfxSlider.addEventListener('input', (event) => {
+    //     const sliderValue = parseFloat(event.target.value);
+    //     const scaledSfxVolume = scaleToAudioVolume(sliderValue);
 
-        if (window.globalAudio) {
-            window.globalAudio.volume = scaledSfxVolume;
-            sfxValue.innerHTML = Math.round(sliderValue);
-        }
+    //     if (window.globalAudio) {
+    //         window.globalAudio.volume = scaledSfxVolume;
+    //         sfxValue.innerHTML = Math.round(sliderValue);
+    //     }
 
-        // save local storage
-        localStorage.setItem('sfxVolumeScaled', scaledSfxVolume);
-    });
+    //     // save local storage
+    //     localStorage.setItem('sfxVolumeScaled', scaledSfxVolume);
+    // });
 });
